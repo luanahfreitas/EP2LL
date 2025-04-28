9137ea24957c6ac5c6f5f9e117490fed702f5ac7
luanahfreitas <luanahughesfreitas@gmail.com>
2025-04-28 18:20:32 -0300

    commit luana ex2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,18 @@ def rolar_dados(n):
         dado = random.randint(1,6)
         lista_dados.append(dado)
     return lista_dados
+
+def guardar_dado(rolados,estoque,indice_dado):
+    resposta = []
+    novo_estoque = []
+
+    i = 0
+    while i < len(estoque):
+        novo_estoque.append(estoque[i])
+        i+=1
+
+    del(rolados[indice_dado])
+    novo_estoque.append(rolados[indice_dado])
+    resposta = [rolados,novo_estoque]
+    return resposta
+  
