c093f85ff0d1b72b0ba51f86391bd49670f67035
laurapittoli <lauravpp@al.insper.edu.br>
2025-04-28 19:03:29 -0300

    commit laura ex4

--- funcoes.py
+++ funcoes.py
@@ -24,10 +24,46 @@ def guardar_dado(rolados,estoque,indice_dado):
 def remover_dado(rolados,estoque,num):
     #num= número inteiro que representa o indice do dado a ser removido
     resposta = []
-    
+
     rolados.append(estoque[num])
 
     del(estoque[num])
 
     resposta = [rolados,estoque]
     return resposta
+
+def calcula_pontos_regra_simples(faces):
+    soma1=0
+    soma2=0
+    soma3=0
+    soma4=0
+    soma5=0
+    soma6=0
+    for i in faces:
+        if i == 1:
+            soma1+=1
+        if i == 2:
+            soma2+=2
+        if i == 3:
+            soma3+=3
+        if i == 4:
+            soma4+=4
+        if i == 5:
+            soma5+=5
+        if i == 6:
+            soma6+=6
+    
+    dic={}
+    dic[1] = soma1
+    dic[2] = soma2
+    dic[3] = soma3
+    dic[4] = soma4
+    dic[5] = soma5
+    dic[6] = soma6
+
+    return dic
+
+        
+        
+        
+        
