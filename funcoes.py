f876d7b2115d8de96cc9ea6bf0a9f5a64cfd7ffa
luanahfreitas <luanahughesfreitas@gmail.com>
2025-04-28 16:45:18 -0300

    commit luana ex1

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,8 @@
-teste
+import random
+
+def rolar_dados(n):
+    lista_d = []
+    while len(lista_dados) != n:
+        dado = random.randint(1,6)
+        lista_dados.append(dado)
+    return lista_d
